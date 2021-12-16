d52379a29c14d807f6e02bbf1fc898259a42e788
Omid Heydarzadeh <omidheydarzadeh510@gmail.com>
2021-12-16 19:49:02 +0330

    Variables in Swift.

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// variables in Swift :
+
+/*
+ 1.Int : Integers.
+         ِEX:  ...,-3,-2,-1,0,1,2,3,...
+ 
+ 2.Float : Decimal Numbers.
+         EX:    1.23, 4.5, 8.234, 9.23
+ 
+ 3.Double : Decimal Numbers.
+ ( It has a higher Capacity than the Float. )
+         EX:   2.345567875, 5.3421, 7.3421
+ 
+ 4.Boll : It has Two Values, True or False. (Boolean)
+ 
+ 5.String : Applies to Text and Strings.
+ ( Inside the double quotation mark we write the text or string. )
+         EX:   "Omid", "Xcode", "Helloworld", "apple"
+ 
+ 6.Character : The meaning is the same as the usual character.
+ */
